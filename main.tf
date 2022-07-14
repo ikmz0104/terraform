168f2e7df37489a39eeb35789f430be66963ac7d
ikmz <ikmz0104.official@gmail.com>
2022-07-14 22:28:45 +0900

    EC2インスタンス起動

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  profile = "terraform" //IAM User
+  region  = "ap-northeast-1"
+}
+
+//EC2インスタンスを起動
+resource "aws_instance" "hello-world" {
+  ami           = "ami-0b7546e839d7ace12"
+  instance_type = "t2.micro"
+}
